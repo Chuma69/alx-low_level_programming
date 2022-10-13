5c6f4676c982c663ce6fb93a7040a9378fb13df0
Chuma69 <raymondonwuoku@gmail.com>
2022-10-13 16:46:56 -0700

    my commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/*inputing the output*/
+	char myVar[] = "\"Programming is like building a multilingual puzzle";
+	/*using puts to output myVar*/
+	puts(myVar);
+
+    return (0);
+}    
