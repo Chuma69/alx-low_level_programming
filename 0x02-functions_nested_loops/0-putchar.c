0418b1b9fac0b19bea6b5db6203634baac7be0d9
Chuma69 <raymondonwuoku@gmail.com>
2022-10-18 17:23:56 -0700

    nawa oo

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *
+ * Return: Always ) (success)
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
